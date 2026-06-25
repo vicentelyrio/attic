b004a6c9aadfebdae0d20f71c46a52168be0e6c8
vicentelyrio <vicentelyrio@gmail.com>
2026-06-25 18:15:20 -0300

    feat(setup): including mantine

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { App } from './App'
-import './styles.css'
+
+import '@mantine/core/styles.css'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider defaultColorScheme="dark">
+        <App />
+      </MantineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
